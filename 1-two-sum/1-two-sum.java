d148fc5c3b735e7c82b6daf1d8012bbd3bfc6989
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-10-10 19:47:25 +0530

    Time: 90 ms (26.86%), Space: 42.6 MB (89.33%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-     // Brute force method   
+     // Brute force method  
+        
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                  int complement = target - nums[i];
