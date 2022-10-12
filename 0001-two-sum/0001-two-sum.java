8e2fcec9a65556f8bef3155b9fabcf315e3c6904
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-10-12 22:14:15 +0530

    Time: 124 ms (10.65%), Space: 44.9 MB (62.90%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+     // Brute force method  
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                 int complement = target - nums[i];
+                
+            if(nums[j] == complement){
+                return new int[] {i , j};
+            }
+          }
+        }
+        throw new IllegalArgumentException("no match found");
+    }
+}
+//Hashmap to reduce time complexity
