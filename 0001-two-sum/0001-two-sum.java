3febe64c9b250333f867437edc25f9b6a542f3bd
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-10-13 16:44:34 +0530

    Time: 5 ms (74.74%), Space: 45.5 MB (42.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,27 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
      // Brute force method  
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                 int complement = target - nums[i];
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                  int complement = target - nums[i];
                 
-            if(nums[j] == complement){
-                return new int[] {i , j};
+//             if(nums[j] == complement){
+//                 return new int[] {i , j};
+//             }
+//           }
+//         }
+//         throw new IllegalArgumentException("no match found");
+//     }
+// }
+//Hashmap to reduce time complexity
+        HashMap<Integer, Integer> num_map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if(num_map.containsKey(complement)){
+                return new int[] {num_map.get(complement), i};
             }
-          }
+            num_map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("no match found");
+        throw new IllegalArgumentException("no match found!");
     }
 }
-//Hashmap to reduce time complexity
