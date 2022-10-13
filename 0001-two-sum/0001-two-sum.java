653f1e51401e146faee6b9d935f5d274776ecf0d
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-10-13 16:46:13 +0530

    Time: 2 ms (95.13%), Space: 42.2 MB (97.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -16,11 +16,11 @@ class Solution {
 //Hashmap to reduce time complexity
         Map<Integer, Integer> num_map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            int complement = target - nums[i];
+            int complement = target-nums[i];
             if(num_map.containsKey(complement)){
-                return new int[] {num_map.get(complement), i};
+                return new int[] {num_map.get(complement),i};
             }
-            num_map.put(nums[i], i);
+            num_map.put(nums[i],i);
         }
         throw new IllegalArgumentException("no match found!");
     }
