629075c4a31f0f839e128f72ed6e16506714a363
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-10-15 22:50:22 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (83.92%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -6,7 +6,7 @@ class Solution {
         
         while(i>=0&&j>=0)
         {
-            if(nums1[i]>=nums2[j])
+            if(nums1[i]>nums2[j])
             {
                 nums1[k--]=nums1[i--];
             }
