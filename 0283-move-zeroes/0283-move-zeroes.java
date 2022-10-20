13bff09b491e0cb98ef223c401ffa20a338cf4a3
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-10-21 01:27:54 +0530

    Time: 3 ms (64.30%), Space: 54.8 MB (42.76%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
+        }
+    }
+}
