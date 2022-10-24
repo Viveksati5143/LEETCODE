6755c67362efc6b87740f6cd0257c8220fd59c71
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-10-24 18:40:36 +0530

    Time: 2 ms (61.03%), Space: 44.5 MB (29.33%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,13 +1,7 @@
 class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int maxProfit=0;
-        for(int i=1;i<prices.size();i++)
-        {
-            if(prices[i]>prices[i-1]){
-                maxProfit+=prices[i]-prices[i-1];
-            }
-        }   
-        return maxProfit;        
-    }
-};
+    public int maxProfit(int[] prices) {       
+    int maxProfit=0;
+    for(int i=1;i<prices.length;i++)if(prices[i]>prices[i-1])maxProfit+=prices[i]-prices[i-1];
+    return maxProfit;
+    }  
+}
