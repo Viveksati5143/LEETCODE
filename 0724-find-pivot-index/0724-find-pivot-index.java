edddd9b76477b66240cd945285fbbae55ecce332
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-10-28 00:09:28 +0530

    Time: 2 ms (84.39%), Space: 52 MB (72.03%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,38 +1,22 @@
 class Solution {
-    public int pivotIndex(int[] nums)
+    public int pivotIndex(int[] nums) 
     {
         if(nums.length<1)
             return -1;
-        int rsum=0, lsum=0;
-        for(int x:nums)
-            rsum+=x;
-        for(int x=0; x<nums.length; x++)
+        int lsum=0,rsum=0;
+        for(int i=0;i<nums.length;i++)
         {
-            rsum-=nums[x];
-            if(rsum==lsum)
-                return x;
-            lsum+=nums[x];
+            rsum+=nums[i];
+        }     
+        for(int i=0;i<nums.length;i++)
+        {
+            rsum-=nums[i];
+            if(lsum==rsum)
+            {
+                return i;
+            }
+            lsum+=nums[i];
         }
         return -1;
     }
 }
-// class Solution {
-//     public int pivotIndex(int[] nums) {
-//         lsum=0,rsum=0;
-//         for(int i=0;i<nums.length;i++)
-//         {
-//             sum+=nums[i];
-//         }     
-//         for(int i=0;i<nums.length;i++)
-//         {
-//             lsum+=nums[i];
-//             rsum+=nums[i];
-//             if(pv==lsum && pv==rsum)
-//             {
-//                 return pv;
-//             }
-//             else return -1; 
-//         }
-//         return pivotIndex;
-//     }
-// }
