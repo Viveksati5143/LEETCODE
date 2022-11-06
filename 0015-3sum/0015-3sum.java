ebb2f7ce5411c4e55a05d120a0eb394596eda650
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-11-06 23:55:40 +0530

    Time: 33 ms (71.62%), Space: 46.2 MB (97.43%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,6 +5,7 @@ class Solution {
         Arrays.sort(nums);
         for(int i=0;i<nums.length-2;i++)
         {
+            if (i > 0 && nums[i] == nums[i - 1])continue;
             int j=i+1;
             int k=nums.length-1;
             while(j<k)
