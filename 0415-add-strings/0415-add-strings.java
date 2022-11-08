a0876561bf7cba58693caef6af4fc374c0ba7c8e
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-11-09 00:28:37 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (90.36%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -4,12 +4,10 @@ class Solution {
         int i=num1.length() -1;
         int j=num2.length() -1;
         int carry=0;
-
         while(i>=0 || j>=0)
         {
             int n1 = (i<0)?0:num1.charAt(i)-'0';
             int n2 = (j<0)?0:num2.charAt(j)-'0';
-
             int sum=n1+n2+carry;
             result.append(sum%10);
             carry=sum/10;
