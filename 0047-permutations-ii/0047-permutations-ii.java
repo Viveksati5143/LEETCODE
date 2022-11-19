254c74f010bfce6ef96043b42b266bd4c6610c74
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-11-20 02:27:07 +0530

    Time: 4 ms (81.60%), Space: 48 MB (59.53%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {   
+        List<List<Integer>> res = new ArrayList<>(); //Powerset 
+        Arrays.sort(nums);
+        Permutations(nums,new ArrayList(),res, new boolean[nums.length]);
+        return res;
+    }
+    
+    void Permutations(int[] nums,List<Integer> curr,List<List<Integer>> res,boolean [] used){
+        if(curr.size()==nums.length) res.add(new ArrayList(curr));//[]
+        else{
+            for(int n=0;n<nums.length;n++){//1,2,3
+                if(used[n] || n > 0 && nums[n] == nums[n-1] && !used[n - 1]) continue;
+                used[n]=true;
+                curr.add(nums[n]);
+                Permutations(nums,curr,res,used);//-------
+                used[n]=false;
+                curr.remove(curr.size()-1);
+            }
+        }       
+    }
+}
