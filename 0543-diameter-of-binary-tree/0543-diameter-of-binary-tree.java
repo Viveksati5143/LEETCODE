2a26b5e09ba5cc13bb2ee26c3ea913cc7201172d
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-11-28 23:25:47 +0530

    Time: 1 ms (80.34%), Space: 43.1 MB (58.52%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,19 +13,38 @@
  *     }
  * }
  */
-class Solution {        
+// class Solution {        
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         if(root == null) return 0;
+// 		int dia1 = diameterOfBinaryTree(root.left);
+// 		int dia2 = diameterOfBinaryTree(root.right);
+// 		int dia3 = Height(root.left) + Height(root.right);		
+// 		return Math.max(dia3, Math.max(dia1, dia2));        
+//     }
+//     public static int Height(TreeNode root) {
+// 		if(root == null) return 0;
+// 		int leftHeight = Height(root.left);
+// 		int rightHeight = Height(root.right);
+// 		int myHeight = Math.max(leftHeight, rightHeight) + 1;
+// 		return myHeight;
+//     }
+// }
+public class Solution {
+    int max = 0;
+    
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
-		int dia1 = diameterOfBinaryTree(root.left);
-		int dia2 = diameterOfBinaryTree(root.right);
-		int dia3 = Height(root.left) + Height(root.right);		
-		return Math.max(dia3, Math.max(dia1, dia2));        
+        maxDepth(root);
+        return max;
     }
-    public static int Height(TreeNode root) {
-		if(root == null) return 0;
-		int leftHeight = Height(root.left);
-		int rightHeight = Height(root.right);
-		int myHeight = Math.max(leftHeight, rightHeight) + 1;
-		return myHeight;
+    
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        max = Math.max(max, left + right);
+        
+        return Math.max(left, right) + 1;
     }
 }
