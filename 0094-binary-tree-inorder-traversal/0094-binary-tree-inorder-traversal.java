beb8b97648f5b7ab7accce5f906e091c748218d5
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-12-01 21:49:18 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (90.31%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,17 +13,37 @@
  *     }
  * }
  */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        IT(root, result);
-        return result;
-    }
-    public void IT(TreeNode root, List<Integer> result) {
-        if (root != null) {
-            IT(root.left, result);
-            result.add(root.val);
-            IT(root.right, result);
+        if(root == null) return new ArrayList<Integer>();
+        List<Integer> res = new ArrayList<Integer>();
+        TreeNode pre = null;
+        while(root != null){
+        	if(root.left == null){
+        		res.add(root.val);
+        		root = root.right;
+        	}else{
+        		pre = root.left;
+        		while(pre.right != null && pre.right != root){
+        			pre = pre.right;
+        		}
+        		if(pre.right == null){
+        			pre.right = root;
+        			root = root.left;
+        		}else{
+        			pre.right = null;
+        			res.add(root.val);
+        			root = root.right;
+        		}
+        	}
         }
+        return res;
     }
 }
