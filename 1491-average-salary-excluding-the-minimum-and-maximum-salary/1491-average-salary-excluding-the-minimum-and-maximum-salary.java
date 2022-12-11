df1beed8ef04cc9e94b768205c345df073dd3db6
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-12-11 20:01:39 +0530

    Time: 1 ms (39.33%), Space: 42.3 MB (10.40%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,13 +1,21 @@
+// class Solution {
+//     public double average(int[] salary) {
+//         double sum = 0d;
+//         int min = Integer.MAX_VALUE;
+//         int max = Integer.MIN_VALUE;
+//         for (int s : salary){
+//             max = Math.max(max, s);
+//             min = Math.min(min, s);
+//             sum += s;
+//         }
+//         return (sum - max - min) / (salary.length - 2);
+//     }
+// }
 class Solution {
-    public double average(int[] salary) {
-        double sum = 0d;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (int s : salary){
-            max = Math.max(max, s);
-            min = Math.min(min, s);
-            sum += s;
-        }
-        return (sum - max - min) / (salary.length - 2);
+    public double average(int[] salary){
+        double res = 0;
+        Arrays.sort(salary);
+        for(int i=1; i<salary.length-1; i++) res += salary[i];
+        return res/(salary.length-2);
     }
 }
