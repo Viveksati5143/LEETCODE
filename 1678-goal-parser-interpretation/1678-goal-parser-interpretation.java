b2ae7b43c5c88f0203916d2bd53f2c65adf6bb6d
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-12-17 23:14:37 +0530

    Time: 1 ms (93.49%), Space: 42.5 MB (23.63%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -4,10 +4,16 @@ class Solution {
 	for(int i = 0 ; i < command.length() ; i++){
 		if(command.charAt(i)=='G') S.append('G');
 		if(command.charAt(i)=='('){
-			if(command.charAt(i+1)==')') {S.append('o'); i++;}
-			else{S.append("al"); i = i + 3;}
+            if(command.charAt(i+1)==')'){
+                S.append('o'); 
+                i++;
+            }
+			else{
+                S.append("al");
+                i = i + 3;
+            }
 		}
 	}
-	return S.toString();
-}
+        return S.toString();
+    }
 }
