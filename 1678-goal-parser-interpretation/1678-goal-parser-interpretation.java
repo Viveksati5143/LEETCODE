0c20c69a20195503bb2c8cef772bf3a49f78b140
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-12-17 22:08:16 +0530

    Time: 1 ms (93.49%), Space: 42.4 MB (23.63%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String interpret(String command) {
+    StringBuilder S = new StringBuilder(command.length());
+	for(int i = 0 ; i < command.length() ; i++){
+		if(command.charAt(i)=='G') S.append('G');
+		if(command.charAt(i)=='('){
+			if(command.charAt(i+1)==')') {S.append('o'); i++;}
+			else{S.append("al"); i = i + 3;}
+		}
+	}
+	return S.toString();
+}
+}
