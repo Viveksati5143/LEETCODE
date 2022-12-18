6c083ea8a4a4a46b611a4ac6b0521d26308b2ca3
VIVEK SATI <100852245+Viveksati5143@users.noreply.github.com>
2022-12-18 12:32:07 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (97.73%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,9 @@
 class Solution {
     public String toLowerCase(String str) {
-        return str.toLowerCase();
+    char[] c = str.toCharArray();
+        for (int i = 0; i < c.length; i++)
+            if ('A' <= c[i] && c[i] <= 'Z')
+                c[i] = (char) (c[i] - 'C' + 'c');
+        return new String(c);
     }
 }
