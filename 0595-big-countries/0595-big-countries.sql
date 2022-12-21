7f765f15b125e9a190a50011cb1c6abd22e265dd
VIVEK SATI <viveksati2002@gmail.com>
2022-12-21 18:11:38 +0530

    Time: 301 ms (99.97%), Space: 0B (100.00%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,1 +1,7 @@
-Select name, population, area from World where area >= 3000000 or population >= 25000000;
+#OR
+# Select name, population, area from World where area >= 3000000 or population >= 25000000;
+
+#UNION
+Select name, population, area from World where area >= 3000000
+UNION
+Select name, population, area from World where population >= 25000000;
