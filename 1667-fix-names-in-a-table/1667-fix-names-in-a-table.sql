2fae4b2b605fc0dfe4096040804964b6cc9c04df
VIVEK SATI <viveksati2002@gmail.com>
2022-12-29 02:20:27 +0530

    Time: 1083 ms (26.20%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,4 +1,7 @@
-select user_id , concat(upper(substr(name,1,1)),lower(substr(name,2))) as name 
+select user_id ,
+concat(upper(substr(name,1,1)),
+       lower(substr(name,2))) 
+       as name 
 from Users
 order by
 user_id;
