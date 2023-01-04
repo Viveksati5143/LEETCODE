ab2515066a52d88f3f8ad0735fc44fb297f10ec3
VIVEK SATI <viveksati2002@gmail.com>
2023-01-05 02:02:30 +0530

    Time: 3 ms (31.22%), Space: 42.9 MB (6.51%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -2,8 +2,8 @@ class Solution {
      public int numJewelsInStones(String jewels, String stones){
         int res = 0;
         Set ans = new HashSet();
-        for (char j: jewels.toCharArray()) ans.add(j);
-        for (char s: stones.toCharArray()) if (ans.contains(s)) res++;
+        for(char j: jewels.toCharArray()) ans.add(j);
+        for(char s: stones.toCharArray()) if(ans.contains(s)) res++;
         return res;
     }
 }
