a0d1fee27cb3471319fde7a3c0bd342b5b8ff09f
VIVEK SATI <viveksati2002@gmail.com>
2023-01-12 03:07:12 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (98.28%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,8 +1,7 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
         return reverseListInt(head,null);
-    }
-            
+    }            
         private ListNode reverseListInt(ListNode head, ListNode newNode){
             if(head==null) return newNode;
             ListNode next = head.next;
