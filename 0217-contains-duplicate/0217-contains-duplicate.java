06c82f0fed19ef40e9cb808e768713a2e79bc0a8
VIVEK SATI <viveksati2002@gmail.com>
2023-01-24 03:19:26 +0530

    Time: 51 ms (5.22%), Space: 71.3 MB (9.76%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]) return true;
-        }
+        for(int i=1;i<nums.length;i++) if(nums[i]==nums[i-1]) return true;
         return false;        
     }
 }
