5fe750d9220703c21b0ee237977230d1a76e4e2d
VIVEK SATI <viveksati2002@gmail.com>
2023-01-24 12:41:13 +0530

    Time: 2 ms (41.50%), Space: 73.7 MB (21.18%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,21 +1,11 @@
 class Solution {
-//     public int maxSubArray(int[] nums) {
-//         int csum = nums[0];
-//         int osum = nums[0];        
-//         for(int i=1;i<nums.length;i++){
-//             if(csum>=0) csum += nums[i]; 
-//             else csum = nums[i];
-//             if(csum > osum) osum = csum;            
-//         }
-//         return osum;
-//     }
-// }
-    public static int maxSubArray(int[] nums){
-        int max=nums[0], maxEnding=nums[0];
-        for(int i=1;i<nums.length;++i){
-            maxEnding= Math.max(maxEnding+nums[i],nums[i]);
-            max=Math.max(max, maxEnding);	
+    public int maxSubArray(int[] nums) {
+        int lsum=nums[0], csum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(csum>=0) csum+=nums[i];
+            else csum = nums[i];
+            if(csum>lsum) lsum=csum;
         }
-        return max;
+        return lsum;
     }
 }
