4aa2d73f28879e6e7091cb42f8524420ff2872ec
VIVEK SATI <viveksati2002@gmail.com>
2023-02-05 20:32:45 +0530

    Time: 1 ms (88.66%), Space: 39.3 MB (87.27%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    int op(int n){
+        int ans = 0;
+        while(n!=0){
+            int temp = n%10;
+            ans += (temp*temp);
+            n/=10; 
+        }
+        return ans;
+    }
+    public boolean isHappy(int n) {
+        int slow=op(n), fast=op(op(n));
+        while(slow!=fast){
+            slow=op(slow);
+            fast=op(op(fast));
+        }
+        return(slow==1);
+    }
+}
