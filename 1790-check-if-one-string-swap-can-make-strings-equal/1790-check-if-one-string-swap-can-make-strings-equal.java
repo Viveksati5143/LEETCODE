1a4ef4db0312f220dac1b73380c2618221020192
VIVEK SATI <viveksati2002@gmail.com>
2023-02-06 02:39:21 +0530

    Time: 1 ms (68.83%), Space: 40.1 MB (97.86%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        int[] s1Array = new int[26];
+        int[] s2Array = new int[26];
+        int res = 0;
+        for(int i = 0;i<s1.length();i++){
+            char x = s1.charAt(i);
+            char y = s2.charAt(i);
+            if(x != y) res++;
+            if(res > 2) return false;
+            s1Array[x -'a']++;
+            s2Array[y -'a']++;
+      }
+      return Arrays.equals(s1Array, s2Array);
+    }
+}
