631a1490e6e49f88fb603dff9dfc62fbb4f41691
VIVEK SATI <viveksati2002@gmail.com>
2023-02-06 02:44:45 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (73.68%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,10 @@
+class Solution {
+    public List<Integer> list = new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+
+        if(root == null) return list;        
+        list.add(root.val);
+        for(Node node: root.children) preorder(node);                
+        return list;
+    }
+}
