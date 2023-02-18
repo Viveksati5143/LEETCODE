245bcd8f0760fb0a21c5e08e6de879d3b6bc08ce
VIVEK SATI <viveksati2002@gmail.com>
2023-02-18 19:16:28 +0530

    Time: 1 ms (68.18%), Space: 40.3 MB (84.69%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -3,7 +3,7 @@ class Solution {
         int[] s1Array = new int[26];
         int[] s2Array = new int[26];
         int res = 0;
-        for(int i = 0;i<s1.length();i++){
+        for(int i = 0;i<s1.length();i++) {
             char x = s1.charAt(i);
             char y = s2.charAt(i);
             if(x != y) res++;
