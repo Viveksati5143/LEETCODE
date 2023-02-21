827faa9f8e8633a6421f2bb9d8b3256cb9cd9e26
VIVEK SATI <viveksati2002@gmail.com>
2023-02-22 03:53:07 +0530

    Time: 2 ms (17.30%), Space: 41.5 MB (8.24%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,8 +1,8 @@
 class Solution {
     public int addDigits(int num) {
-        while(num >= 10) {
+        while (num >= 10) {
             int sum = 0;
-            while(num > 0) {
+            while (num > 0) {
                 sum += num % 10;
                 num /= 10;
             }
