313534f02bc3aaef4162f208429c9c3e4ad5f02d
VIVEK SATI <viveksati2002@gmail.com>
2023-02-24 01:49:19 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (6.05%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,11 +1,13 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int w = 0;
+        int res = 0;
         for(int i=0;i<accounts.length;i++){
-            int w2 = 0;        
-            for(int j=0;j<accounts[i].length;j++) w2 += accounts[i][j];
-            w = Math.max(w,w2);
+            int sum = 0;
+            for(int j=0;j<accounts[i].length;j++){
+                sum += accounts[i][j];
             }
-        return w; 
+            if(sum>res) res = sum;                
+        }
+        return res;
     }
 }
