8f0113ae1de3c13426b72eb034868e37862a6145
VIVEK SATI <viveksati2002@gmail.com>
2023-02-25 01:04:30 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (15.39%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -3,9 +3,7 @@ class Solution {
         int res = 0;
         for(int i=0;i<accounts.length;i++){
             int sum = 0;
-            for(int j=0;j<accounts[i].length;j++){
-                sum += accounts[i][j];
-            }
+            for(int j=0;j<accounts[i].length;j++) sum += accounts[i][j];
             if(sum>res) res = sum;                
         }
         return res;
