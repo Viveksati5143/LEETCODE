1dc2fba9aa10fcf1c714352d26095e1b14e52449
VIVEK SATI <viveksati2002@gmail.com>
2023-02-27 20:55:33 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (17.28%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int start=0, end=arr.length-1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]>arr[mid+1]) end = mid;
+            else start = mid+1;
+        }
+        return start;        
+    }
+}
