1730695ccc81acf04a7d35f72c4dcc9bb83bf0db
VIVEK SATI <viveksati2002@gmail.com>
2023-03-06 22:06:10 +0530

    Time: 2 ms (84.41%), Space: 40.4 MB (18.11%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,10 +1,19 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int i = 1;
-        while(n>=i){
-            n -= i;
-            i++;
+        // int i = 1;
+        // while(n>=i){     //Brute Force
+        //     n -= i;
+        //     i++;
+        // }
+        // return i-1;
+        long start = 0;
+        long end = n;
+        while(start<=end){
+            long mid = start + (end-start)/2;    //Binary Search
+            if(mid*(mid+1)/2==n) return (int)mid;
+            else if(mid*(mid+1)/2<n)start = mid + 1;
+            else end = mid -1;
         }
-        return i-1;
+        return (int)end;
     }
 }
