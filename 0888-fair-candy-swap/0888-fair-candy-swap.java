3f9c29935e4d442e9ca141b8d0fa5cbfcd61dba6
VIVEK SATI <viveksati2002@gmail.com>
2023-03-09 15:58:26 +0530

    Time: 18 ms (46.29%), Space: 44.6 MB (20.53%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.java
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -4,11 +4,21 @@ class Solution {
         for(int i=0;i<alice.length;i++) A += alice[i]; 
         for(int j=0;j<bob.length;j++) B += bob[j]; 
         int diff = (A-B)/2;
-        for(int i=0;i<alice.length;i++){
-            for(int j=0;j<bob.length;j++){
-                if(alice[i]-bob[j]==diff) return new int[]{alice[i],bob[j]};
-            }
+        Arrays.sort(alice);
+        for(int num:bob){
+            if(binarySearch(alice,num+diff) != -1) return new int[]{num+diff,num};
         }
         return null;
+    }
+    int binarySearch(int[] arr,int target){ 
+        int s=0, e=arr.length-1;
+        
+        while(s<=e){
+            int mid = s+ (e-s)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]<target) s=mid+1;
+            else e=mid-1;            
+        }
+        return -1;
     }
 }
