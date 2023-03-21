04ceb8d07ed917fb90ac403bf1bda051c30a1b4f
VIVEK SATI <viveksati2002@gmail.com>
2023-03-22 00:18:58 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (14.46%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,7 +1,5 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double Kelvin = celsius + 273.15;
-        double Fahrenheit = celsius * 1.80 + 32.00;
-        return new double[]{Kelvin, Fahrenheit};  
+        return new double[]{celsius + 273.15, celsius * 1.80 + 32.00};  
     }
 }
