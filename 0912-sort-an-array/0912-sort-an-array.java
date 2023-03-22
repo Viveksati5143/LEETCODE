8a161a3c231bfc9bbb5ad94ff3c84afe63b4efb9
VIVEK SATI <viveksati2002@gmail.com>
2023-03-23 01:33:25 +0530

    Time: 36 ms (54.60%), Space: 53.5 MB (26.21%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,41 +1,43 @@
 class Solution {
     public int[] sortArray(int[] arr) {
-        arr = mergeSort(arr);
+        mergeSortInPlace(arr, 0, arr.length);
         return arr;
     }
-    int[] mergeSort(int[] arr){
-        if (arr.length==1) return arr;
+    static void mergeSortInPlace(int[] arr, int s, int e){
+        if (e-s==1) return;
 
-        int mid = arr.length/2;
-        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
-        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
-        return merge(left, right);
+        int mid = (s+e)/2;
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+        mergeInPlace(arr, s, mid, e);
     }
 
-    int[] merge(int[] first, int[] second){
-        int [] mix = new int[first.length+ second.length];
-        int i=0, j=0, k=0;
-        while (i< first.length && j< second.length){
-            if(first[i]<second[j]){
-                mix[k] = first[i];
+    static void mergeInPlace(int[] arr, int s, int m, int e){
+        int [] mix = new int[e-s];
+        int i=s, j=m, k=0;
+        while (i<m && j<e){
+            if(arr[i]<arr[j]){
+                mix[k] = arr[i];
                 i++;
             }
             else{
-                mix[k] = second[j];
+                mix[k] = arr[j];
                 j++;
             }
             k++;
         }
-        while (i< first.length){
-            mix[k] = first[i];
+        while (i<m){
+            mix[k] = arr[i];
             i++;
             k++;
         }
-        while (j< second.length){
-            mix[k] = second[j];
+        while (j<e){
+            mix[k] = arr[j];
             j++;
             k++;
         }
-        return mix;
+        for (int l = 0; l < mix.length; l++) {
+            arr[s+l] = mix[l];
+        }
     }
 }
