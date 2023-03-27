caa5fe744635d4d8dc90f608221937b494386de6
VIVEK SATI <viveksati2002@gmail.com>
2023-03-28 00:56:54 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (41.32%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,5 +1,23 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+        boolean res = true;
+        for(int i = 2; i < n-1; i++){
+            if(!check(Integer.toString(n,i))){
+                res = false;
+                break;
+            }
+        }
+        return res;
+    }
+    public boolean check(String n){
+        int l = 0;
+        int r = n.length()-1;
+        while(l<r){
+            if(n.charAt(l)!=n.charAt(r))
+                return false;
+            l++;
+            r--;
+        }
+        return true;
     }
 }
