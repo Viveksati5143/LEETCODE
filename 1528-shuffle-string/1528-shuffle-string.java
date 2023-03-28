2d2a064fbcbde004f0759deddbe155a78bcddaa0
VIVEK SATI <viveksati2002@gmail.com>
2023-03-28 11:20:51 +0530

    Time: 3 ms (29.52%), Space: 42.9 MB (37.34%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,9 +1,9 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        char[] res = s.toCharArray();
+        String[] res = new String[s.length()];
         for (int i = 0; i < indices.length; i++) {
-            res[indices[i]] = s.charAt(i);
+            res[indices[i]] = String.valueOf(s.charAt(i));
         }
-        return new String(res);
+        return String.join("", res);
     }
 }
