20c32ae637a442bce49ff34af538dc8b7cd232c8
VIVEK SATI <viveksati2002@gmail.com>
2023-04-02 11:44:50 +0530

    Time: 484 ms (20.85%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,3 +1,4 @@
-Select machine_id, round((sum(case when activity_type = 'end' then timestamp end) - sum(case when activity_type='start' then timestamp end))/count(distinct process_id), 3) as processing_time 
+Select machine_id, 
+round((sum(case when activity_type='start' then -1*timestamp else timestamp end))/count(distinct process_id), 3) as processing_time 
 from Activity
 group by machine_id;
