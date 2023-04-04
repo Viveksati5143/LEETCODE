dd0e4c1dd78f94ba2a314bd0c1e5d1bcfdbeaea9
VIVEK SATI <viveksati2002@gmail.com>
2023-04-04 09:16:27 +0530

    Time: 1183 ms (79.02%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,6 @@
+Select s.user_id, 
+round(avg(case when action='confirmed' then 1 else 0 end), 2) as confirmation_rate 
+from Signups s
+left join Confirmations c
+on s.user_id = c.user_id
+group by s.user_id;
