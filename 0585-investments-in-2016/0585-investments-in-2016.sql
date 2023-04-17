3539239efa97d4d3f67db1981d778d615d3a5640
VIVEK SATI <viveksati2002@gmail.com>
2023-04-17 20:45:24 +0530

    Time: 856 ms (85.89%), Space: 0B (100.00%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,5 +1,6 @@
-Select round(sum(tiv_2016), 2) aS tiv_2016 from insurance 
-where pid in 
-(Select pid from insurance group by lat, lon having count(*) = 1) 
-and pid not in
-(Select pid from insurance group by tiv_2015 having count(*) = 1)
+Select round(sum(tiv_2016), 2) aS tiv_2016 
+from insurance 
+where pid in (
+    Select pid from insurance group by lat, lon having count(*) = 1) 
+and pid not in (
+    Select pid from insurance group by tiv_2015 having count(*) = 1);
