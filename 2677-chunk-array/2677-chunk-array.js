60970a65185b635b60e9d3146abbbf3ec30c9cb4
VIVEK SATI <viveksati2002@gmail.com>
2023-05-25 05:34:59 +0530

    Time: 63 ms (63.27%), Space: 44.9 MB (22.88%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (_, index) =>
+    arr.slice(index * size, index * size + size)
+  );
+
