5b86cb9677702727d59c3b0a76eb960e1d02275d
VIVEK SATI <viveksati2002@gmail.com>
2023-06-15 09:55:24 +0530

    Time: 69 ms (52.76%), Space: 42.1 MB (47.61%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+  return Promise.all([promise1, promise2]).then((values) => {
+    const sum = values.reduce((acc, curr) => acc + curr, 0);
+    return sum;
+  });  
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
